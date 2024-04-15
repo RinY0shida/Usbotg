0361c452c02d88cb2a32d7f52106666d03bea7c5
RinY0shida <tororo1219@gmail.com>
2024-04-16 01:43:01 +0900

    ✨ complete sending and receiving

--- Ubuntu/sending-and-receiving.cc
+++ Ubuntu/sending-and-receiving.cc
@@ -3,23 +3,43 @@
 #include <string>
 
 int main() {
-    std::fstream serial("/dev/ttyUSB0", std::ios::in | std::ios::out | std::ios::binary);
+    int flag;
+    std::string moji;
+    std::fstream serial("/dev/ttyACM1", std::ios::in | std::ios::out | std::ios::binary);
+    if(serial.is_open()){
+        std::cout << "Serial port is open" << std::endl;
+    } else {
+        std::cout << "Serial port is not open" << std::endl;
+    }
+    while(true){
+        std::cin >> moji;
+        serial << moji << std::endl;
+         // std::cout << "moji: " << moji << std::endl;
+        flag = true;
+        while(flag){
+            if(!std::getline(serial, moji)){
+                std::cout << "stm32_receive:" << moji << std::endl;
+                flag = false;
+            }
+        }
+    }
 
      //sending data to the serial port
-    if (serial.is_open()) {
-        serial << "Hello, World!" << std::endl;
+    // if (serial.is_open()) {
+    //     serial << "Hello, World!" << std::endl;
 
-         //reading data from the serial port
-        std::string line;
+    //      //reading data from the serial port
+    //     std::string line;
 
-        while (std::getline(serial, line)) {
-            std::cout << line << std::endl;
-        }
+    //     while (std::getline(serial, line)) {
+    //         std::cout << line << std::endl;
+    //     }
 
-    } 
-    else {
-        std::cerr << "Error: Could not open serial port." << std::endl;
-    }
+    // } 
+    // else {
+    //     std::cerr << "Error: Could not open serial port." << std::endl;
+    // }
+
 
      //reading data from the serial port
      
